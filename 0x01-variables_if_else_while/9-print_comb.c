7d46ef1d62f65622f9e02033306e635c7bc75688
STARR44 <okekeesther100@gmail.com>
2023-10-11 06:53:32 -0700

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints all possible combination of single digit
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
