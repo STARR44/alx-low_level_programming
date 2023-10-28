8a06b1dac5fd753176e6e6463f150f6f11d078f9
STARR44 <okekeesther100@gmail.com>
2023-10-28 03:53:53 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _puts - Write a function that prints a string, followed by a new line, to stdout
+ * _puts - A function that prints a string, followed by a new line, to stdout
  * @str: string to be printed
  * Return: void
  */
