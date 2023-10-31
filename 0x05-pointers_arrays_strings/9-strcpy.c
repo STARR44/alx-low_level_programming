d5a2a2912c5eafbc07f0031b133d4a6c8191576f
STARR44 <okekeesther100@gmail.com>
2023-10-31 06:26:22 -0700

    task 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,8 +11,10 @@ char *_strcpy(char *dest, char *src)
 {
 	int p;
 
-	for (p = 0; src[p] != '\0'; p++);
-	dest[p] = src[p];
+	for (p = 0; src[p] != '\0'; p++)
+	{
+		dest[p] = src[p];
+	}
 	dest[p] = '\0';
 	return (dest);
 }
