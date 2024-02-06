d9aa6e20f4ccdc71434d0eb5787fb087d56195ef
STARR44 <okekeesther100@gmail.com>
2024-02-06 06:19:28 -0800

    created an array of char then initialized with c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/*
+ * create_array - creates an array
+ * @size: calculates the length of the array
+ * @c: character to store in array
+ * Return: pointer to the array
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char* new_array;
+	unsigned int p;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	new_array = (char*)malloc(size * sizeof(char));
+	if (new_array == NULL)
+	{
+		return (NULL);
+	}
+	for(p = 0; p < size; p++)
+		new_array[p] = c;
+	return (new_array);
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,1 @@
+tasks on mallocs and free
