99386407ad244ef08358e896d79cdbff587006ad
STARR44 <okekeesther100@gmail.com>
2024-02-06 07:04:45 -0800

    made chnages to my betty documentation

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /*
- * create_array - creates an array
- * @size: calculates the length of the array
+ * create_array - A function that creates an array of characters
+ * @size: size of the array
  * @c: character to store in array
  * Return: pointer to the array
  */
 
 char *create_array(unsigned int size, char c)
 {
-	char* new_array;
+	char  *new_array;
 	unsigned int p;
 
 	if (size == 0)
 	{
 		return (NULL);
 	}
-	new_array = (char*)malloc(size * sizeof(char));
+	new_array  = (char *)malloc(size * sizeof(char));
 	if (new_array == NULL)
 	{
 		return (NULL);
 	}
-	for(p = 0; p < size; p++)
+	for (p = 0; p < size; p++)
 		new_array[p] = c;
 	return (new_array);
 }
